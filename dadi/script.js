faf7e512b3f95e5d22a0f943110bf51ef158a711
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-13 14:42:31 +0100

    add gioco dei dadi

--- dadi/script.js
+++ dadi/script.js
@@ -1,13 +1,28 @@
 /*
 Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Prima di partire a scrivere codice poniamoci qualche domanda per individuare gli strumenti necessari allo sviluppo.
-Consigli del giorno:
-scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-si ma noi cosa vogliamo fare?
-torniamo a scrivere in italiano
-proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
 */
 
-console.log('dadi');
+// Genero due numeri interi casuali da 1 a 6
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
+}
+
+let userNum = getRandomIntInclusive(1, 6)
+let pcNum = getRandomIntInclusive(1, 6)
+
+console.log(userNum);
+console.log(pcNum);
+ 
+// Controllo quale dei due numeri è maggiore e lo stampo a console
+
+if (userNum > pcNum) {
+    console.log('Hai vinto tu!');
+} else if (userNum < pcNum) {
+    console.log('Ha vinto il pc!');
+} else {
+    console.log('Tu e il pc avete trovato lo stesso numero. Riprova');
+}
--- mail/script.js
+++ mail/script.js
@@ -5,9 +5,9 @@ Chiedi all’utente la sua email, controlla che sia nella lista di chi può acce
 stampa un messaggio appropriato sull’esito del controllo.
 Lo svolgimento va fatto usando un ciclo for è non usando metodi degli array (come includes, per esempio) . Si puó fare??
 eh si!  Basta ragionare un po’.
-:puntare_a_destra: Bonus:
+
+Bonus:
 Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
 Nota: Non è necessario provvedere alla validazione delle email
 */
 
-console.log('mail');
