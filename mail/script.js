88a3d4d5bf40ad2b0044349980f9d7be30d1311b
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-13 13:39:42 +0100

    first commit

---
+++ mail/script.js
@@ -0,0 +1,13 @@
+/*
+Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Lo svolgimento va fatto usando un ciclo for è non usando metodi degli array (come includes, per esempio) . Si puó fare??
+eh si!  Basta ragionare un po’.
+:puntare_a_destra: Bonus:
+Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
+Nota: Non è necessario provvedere alla validazione delle email
+*/
+
+console.log('mail');
