c7d71204cd2c00376f87272db7ed1e95ad5fc88f
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-13 15:10:34 +0100

    add mail bonus script

---
+++ mail/bonus/script.js
@@ -0,0 +1,27 @@
+/*
+Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+
+Bonus:
+Dopo aver svolto l'esercizio con il for fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
+*/
+
+// Creo un'array di email
+mailList = ['luigi@gmail.com', 'marco@hotmail.com', 'simone@gmail.it']
+
+// Prompt per permettere all'utente di inserire la propria email
+
+const userMail = String(prompt('Inserisci la tua email')) 
+
+// Controllo che la mail sia presente nell'array e stampo messaggio di esito
+
+let mailMatch = false;
+
+if (mailList.includes(userMail)) {
+    mailMatch = true;
+    console.log(`Puoi entrare! La tua email è presente nella lista degli invitati!`);
+} else {
+    console.log(`Mi dispiace. La tua email non è presente nella lista degli invitati`);
+}
