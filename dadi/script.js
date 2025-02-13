5e6f25bd1576d959ca984ce50759ccef8b46181c
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-13 16:22:47 +0100

    edit mail bonus script

--- dadi/script.js
+++ dadi/script.js
@@ -1,8 +1,3 @@
-/*
-Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
-*/
-
 // Genero due numeri interi casuali da 1 a 6
 
 function getRandomIntInclusive(min, max) {
--- mail/script.js
+++ mail/script.js
@@ -1,13 +1,3 @@
-/*
-Mail
-Crea una lista di email di invitati ad una festa.
-Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
-stampa un messaggio appropriato sull’esito del controllo.
-
-Bonus:
-Dopo aver svolto l'esercizio con il for fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
-*/
-
 // Creo un'array di email
 mailList = ['luigi@gmail.com', 'marco@hotmail.com', 'simone@gmail.it']
 
