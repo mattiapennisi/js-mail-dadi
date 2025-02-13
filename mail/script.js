61a4a90e955da631a0fad2402d2521d68ff1a7be
mattiapennisi <mattiapennisi0@gmail.com>
2025-02-13 15:03:46 +0100

    add mail script

--- mail/script.js
+++ mail/script.js
@@ -3,11 +3,29 @@ Mail
 Crea una lista di email di invitati ad una festa.
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo.
-Lo svolgimento va fatto usando un ciclo for è non usando metodi degli array (come includes, per esempio) . Si puó fare??
-eh si!  Basta ragionare un po’.
 
 Bonus:
-Dopo aver svolto l'esercizio con il for  fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
-Nota: Non è necessario provvedere alla validazione delle email
+Dopo aver svolto l'esercizio con il for fai una cartella bonus e svolgilo anche anche usando un metodo delle array tipo includes
 */
 
+// Creo un'array di email
+mailList = ['luigi@gmail.com', 'marco@hotmail.com', 'simone@gmail.it']
+
+// Prompt per permettere all'utente di inserire la propria email
+
+const userMail = String(prompt('Inserisci la tua email')) 
+
+// Controllo che la mail sia presente nell'array e stampo messaggio di esito
+
+let mailMatch = false;
+
+for (let i = mailList.length - 1; i >= 0; i--) {
+    if (userMail === mailList[i]) {
+        mailMatch = true;
+        console.log(`Puoi entrare! La tua email è presente nella lista degli invitati!`);
+    }
+}
+
+if (mailMatch == false) {
+    console.log(`Mi dispiace. La tua email non è presente nella lista degli invitati`);
+}
